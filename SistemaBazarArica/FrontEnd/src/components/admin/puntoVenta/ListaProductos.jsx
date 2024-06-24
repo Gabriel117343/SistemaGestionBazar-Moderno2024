18e12964724a6e76ae07cfec2036e996a3dc033b
gabriel <gabriel.soliz@inacapmail.cl>
2024-06-24 13:00:26 -0400

    icon de alerta en producto cuando no hay stock

--- SistemaBazarArica/FrontEnd/src/components/admin/puntoVenta/ListaProductos.jsx
+++ SistemaBazarArica/FrontEnd/src/components/admin/puntoVenta/ListaProductos.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect, useContext } from "react";
 import { MagicMotion } from "react-magic-motion";
-import { toast } from 'react-hot-toast'
+import { toast } from "react-hot-toast";
 import { SidebarContext } from "../../../context/SidebarContext";
 import { CarritoContext } from "../../../context/CarritoContext";
 export const ListaProductos = ({ productos }) => {
@@ -67,60 +67,62 @@ export const ListaProductos = ({ productos }) => {
             // const ImageWithLoading = withLoadingImage((props) => <img {...props} />);
             return (
               <li key={producto?.id} className="producto">
-                <div className="producto">
-                  <div className="pt-0">
-                    {producto.imagen ? (
-                      <img
-                        width="100%"
-                        height="150px"
-                        src={producto.imagen}
-                        alt={`esto es una imagen de un ${producto.nombre}`}
-                      />
-                    ) : (
-                      <img
-                        width="100%"
-                        height="150px"
-                        src="https://ww.idelcosa.com/img/default.jpg"
-                        alt="esta es una imagen por defecto"
-                      />
-                    )}
+                {cantidadCalculada === 0 && (
+                  <div className="icono-informativo">
+                    <i class="bi bi-exclamation-circle"></i>
                   </div>
+                
+                )}
+                
+                
+                <div className="pt-0">
+                  {producto.imagen ? (
+                    <img
+                      width="100%"
+                      height="150px"
+                      src={producto.imagen}
+                      alt={`esto es una imagen de un ${producto.nombre}`}
+                    />
+                  ) : (
+                    <img
+                      width="100%"
+                      height="150px"
+                      src="https://ww.idelcosa.com/img/default.jpg"
+                      alt="esta es una imagen por defecto"
+                    />
+                  )}
+                </div>
 
-                  <div className="p-0 m-0">
-                    <p className="producto__nombre p-0 m-0">
-                      {producto.nombre}
+                <div className="p-0 m-0">
+                  <p className="producto__nombre p-0 m-0">{producto.nombre}</p>
+                  <div className="d-flex justify-content-center">
+                    <p className="p-0 m-0 text-success precio-num">
+                      ${producto.precio}
                     </p>
-                    <div className="d-flex justify-content-center">
-                      <p className="p-0 m-0 text-success precio-num">
-                        ${producto.precio}
+                    <div className="d-flex align-items-center stock-num">
+                      <strong
+                        className={`p-0 m-0 ps-2 d-flex align-items-center`}
+                      >
+                        Stock:
+                      </strong>
+                      <p
+                        className={`${cantidadCalculada === 0 && "text-danger"}`}
+                      >
+                        {cantidadCalculada}
                       </p>
-                      <div className="d-flex align-items-center stock-num">
-                        <strong
-                          className={`p-0 m-0 ps-2 d-flex align-items-center`}
-                        >
-                          Stock:
-                        </strong>
-                        <p
-                          className={`${cantidadCalculada === 0 && "text-danger"}`}
-                        >
-                          {cantidadCalculada}
-                        </p>
-                      </div>
                     </div>
                   </div>
-                  <div className="pt-0 mt-0 btn-agregar">
-                    <button
-                      onClick={() => agregarProducto(producto)}
-                    >
-                      Agregar
-                    </button>
-                  </div>
+                </div>
+                <div className="pt-0 mt-0 btn-agregar">
+                  <button onClick={() => agregarProducto(producto)}>
+                    Agregar
+                  </button>
                 </div>
               </li>
             );
           })}
         </MagicMotion>
-      </ul> 
+      </ul>
       <div className="pt-1">
         {Array.from({ length: totalBotones }, (_, index) => (
           <button
@@ -143,7 +145,6 @@ const SinProductos = () => {
   );
 };
 export const ValidarProductos = ({ productos }) => {
-
   const validacion = productos.length != 1;
 
   return (
