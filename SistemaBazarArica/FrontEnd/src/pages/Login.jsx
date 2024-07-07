153407ccbecb125fe30ae079b11db555ac5e4f20
gabriel <gabriel.soliz@inacapmail.cl>
2024-07-07 16:02:04 -0400

    Corrección de la importacio´n de Login.css

--- SistemaBazarArica/FrontEnd/src/pages/Login.jsx
+++ SistemaBazarArica/FrontEnd/src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import { useState, useId, useContext, useRef } from "react";
-import "./login.css";
+import "./loginPage.css";
 import { Link } from "react-router-dom";
 import { toast } from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
