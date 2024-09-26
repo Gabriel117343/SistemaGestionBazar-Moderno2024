1bc877662523b259b01d40dfa4c3a535361d9dc1
Gabriel S. <gabriel.soliz@inacapmail.cl>
2024-09-25 23:33:20 -0300

    fix bug forwardRef

--- FrontEnd/src/components/shared/InputSearch.jsx
+++ FrontEnd/src/components/shared/InputSearch.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React, { forwardRef} from 'react'
 
-export const InputSearch = (props) => {
+export const InputSearch = forwardRef((props) => {
   return (
     <input ref={props.ref} type="text" id={props.id} className={`form-control ${props.className}`} onChange={props.onChange} {...props}/>
   )
-}
+});
