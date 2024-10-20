f5d4253fdfc1fd05080832c08d699652c7fa69ef
Gabriel S. <gabriel.soliz@inacapmail.cl>
2024-10-20 19:40:06 -0300

    Carga diferida en componentes de Stock

--- FrontEnd/src/pages/admin/Stocks.jsx
+++ FrontEnd/src/pages/admin/Stocks.jsx
@@ -1,21 +1,43 @@
-import { StockSmart } from "../../components/admin/stocks/StockSmart";
+import { Suspense, lazy } from "react";
+import { lazyLoad } from "../../utils/lazyLoad.js";
 import { FaBoxes } from "react-icons/fa";
-import GradualSpacing from "../../components/shared/magic_ui/GradualSpacing";
+
 import "./stylepages.css";
+import Skeleton from "../../components/skeletons/Skeleton.jsx";
+import SkeletonText from "../../components/skeletons/SkeletonText.jsx";
+
+const StockSmart = lazyLoad(
+  "../components/admin/stocks/StockSmart",
+  "StockSmart"
+);
+const GradualSpacing = lazy(
+  () => import("../../components/shared/magic_ui/GradualSpacing.jsx")
+);
+
 export const Stocks = () => {
   return (
     <section className="container-fluid">
       <div className="d-flex align-items-center justify-content-left gap-3  pt-2 pb-2 titulo-page">
-        <div
-          style={{ fontSize: "40px" }}
-          className="d-flex align-items-center p-0 m-0 ms-2"
-        >
-          <FaBoxes />
-        </div>
-        <GradualSpacing text="Stock en Tienda" className="m-0" type="h2" />
+        <Suspense fallback={<h2>Stock en Tienda</h2>}>
+          <div
+            style={{ fontSize: "40px" }}
+            className="d-flex align-items-center p-0 m-0 ms-2"
+          >
+            <FaBoxes />
+          </div>
+          <GradualSpacing text="Stock en Tienda" className="m-0" type="h2" />
+        </Suspense>
       </div>
-
-      <StockSmart />
+      <Suspense
+        fallback={
+          <>
+            <SkeletonText lines={2} gap="8px" />
+            <Skeleton height="500px" width="100%" />
+          </>
+        }
+      >
+        <StockSmart />
+      </Suspense>
     </section>
   );
 };
